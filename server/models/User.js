a7a5a3a3a8bb2215ceddab671f63b8ede074f5f2
John Desrosiers <john.desro@gmail.com>
2021-08-24 07:19:32 -0700

    Update develop script

--- server/models/User.js
+++ server/models/User.js
@@ -19,12 +19,6 @@ const userSchema = new Schema({
     required: true,
     minlength: 5,
   },
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Thought",
-    },
-  ],
 });
 
 userSchema.pre("save", async function (next) {
