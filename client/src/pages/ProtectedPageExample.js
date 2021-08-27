f4a26554a0debff0f75301e8c9380b0beb415df6
John Desrosiers <john.desro@gmail.com>
2021-08-27 06:18:14 -0700

    Add me query to ProtectedPageExample

--- client/src/pages/ProtectedPageExample.js
+++ client/src/pages/ProtectedPageExample.js
@@ -1,11 +1,25 @@
+import { useQuery } from "@apollo/client";
 import { useAuth } from "../util/auth";
+import { ME } from "../util/queries";
+
+const renderDate = (date) =>
+  `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
 
 export default function ProtectedPageExample() {
   const { user } = useAuth();
+  const { data, loading } = useQuery(ME, {
+    // skip cache for demonstration
+    fetchPolicy: "network-only",
+  });
   return (
     <div>
-      {/* TODO: display logged in user's username */}
       <h1>Welcome {user.username}!</h1>
+      <p>
+        Last Login:{" "}
+        {loading
+          ? "Loading..."
+          : data && renderDate(new Date(data.me.lastLogin))}
+      </p>
       <hr />
       <p>Your id is {user._id}</p>
       <p>Your email is {user.email}</p>
---
+++ client/src/util/queries.js
@@ -0,0 +1,12 @@
+import { gql } from "@apollo/client";
+
+export const ME = gql`
+  query me {
+    me {
+      _id
+      lastLogin
+      username
+      email
+    }
+  }
+`;
