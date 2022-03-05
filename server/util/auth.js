d89bc6bf163faafb1dc717d1f51891c45bd586c6
John Desrosiers <john.desro@gmail.com>
2022-03-04 16:15:40 -0800

    Update comment in util/auth.js

--- server/util/auth.js
+++ server/util/auth.js
@@ -22,7 +22,7 @@ const authMiddleware = async ({ req }) => {
   // allows token to be sent via req.body, req.query, or headers
   let token = req.body.token || req.query.token || req.headers.authorization;
 
-  // We split the token string into an array and return actual token
+  // Split the token string into an array and return actual token
   if (req.headers.authorization) {
     token = token.split(" ").pop().trim();
   }
